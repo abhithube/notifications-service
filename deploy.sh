f96f904c9da537b419c7b4b6a53cf62fdeb9d3c2
Abhi Thube <abhimanyuthube@gmail.com>
2020-11-03 00:26:15 -0800

    Configured deploy script to force new deployment after pushing to ECR

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ export PATH=$PATH:$HOME/.local/bin
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 449094484520.dkr.ecr.us-west-2.amazonaws.com
 docker tag insurance-portal/notifications:latest 449094484520.dkr.ecr.us-west-2.amazonaws.com/insurance-portal/notifications:latest
 docker push 449094484520.dkr.ecr.us-west-2.amazonaws.com/insurance-portal/notifications:latest
+aws ecs update-service --cluster insurance-portal-ec2-cluster --region us-west-2 --service notifications-srv --force-new-deployment
